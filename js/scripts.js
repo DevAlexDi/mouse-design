aac6aa1525e230c0f2824f016bce1124e23c1394
unknown <keitmark@mail.ru>
2018-01-31 13:22:59 +0300

    qqq

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-   
+    
+    
+   $('.label-wr').mousedown(function(){
+       $(this).addClass('scaled');
+       //alert('ok');
+   });
+    $('.label-wr').mouseup(function(){
+       $(this).removeClass('scaled');
+   });
     
 
 });
