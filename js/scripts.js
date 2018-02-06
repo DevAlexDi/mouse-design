21c034fa884db4a761c14d3ee164b542dd3acea7
lexa <keitmark@mail.ru>
2018-02-06 15:56:09 +0300

    qq

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     
-    
+    //страница signUp-2.html
    $('.label-wr').mousedown(function(){
        $(this).addClass('scaled');
        //alert('ok');
@@ -8,6 +8,51 @@ $(document).ready(function () {
     $('.label-wr').mouseup(function(){
        $(this).removeClass('scaled');
    });
+    //страница signUp-2.html
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //страница shows.html
+    
+    var hu = $(".current-slider").ionRangeSlider({
+        min: 0,
+        max: 300,
+        from: 150,
+        
+        type: "single",
+        hide_min_max: true,
+        postfix: " km",
+        grid: false,
+        grid_num: 5,
+        onChange: function(){
+           
+        }
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 
 });
