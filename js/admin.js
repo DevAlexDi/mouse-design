3439bac19eedeec80da83816d006aa2744fcac7c
Tatyana Komarova <komarovats93@gmail.com>
2018-07-04 22:58:45 +0300

    Admin Settings

--- js/admin.js
+++ js/admin.js
@@ -2,14 +2,11 @@ $(document).ready(function () {
     $('.admin_page nav .opening .admin_menu_link').click(function(e) {
         e.preventDefault();
         if (!$(this).parent().hasClass('opened')) {
-            $('.admin_page nav .opening.opened .admin_menu_link').removeClass('active');
             $('.admin_page nav .opening').removeClass('opened');
             $(this).parent().addClass('opened');
-            $(this).addClass('active');
         }
         else {
             $(this).parent().removeClass('opened');
-            $(this).removeClass('active');
         }
     });
 });
